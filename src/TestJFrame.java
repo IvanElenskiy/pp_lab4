ca97e3b8d55fe043656a0ddf179af3209eeed864
HelloV <vadim--leo@mail.ru>
2015-04-07 23:32:04 +0300

    Test commit

--- src/TestJFrame.java
+++ src/TestJFrame.java
@@ -4,7 +4,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.swing.JTextField;
 
-
 //Example
 public class TestJFrame {
 	private JTextField arg1 = new JTextField();
